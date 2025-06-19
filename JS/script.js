615e52e7fba23eb846303bd1310349f45493aaca
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-06-19 23:03:55 +0100

    fix: move mobile navigation script inside DOMContentLoaded event

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,10 @@
 ///////////////////////////////////////////////////////////
 // Making mobile navigation work
-document.addEventListener("DOMContentLoaded", function () {
-  const btnNavEl = document.querySelector(".btn-mobile-nav");
-  const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-  btnNavEl.addEventListener("click", function () {
-    headerEl.classList.toggle("nav-open");
-  });
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
 ///////////////////////////////////////////////////////////
